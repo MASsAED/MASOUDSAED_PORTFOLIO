a5d8c8c2c6223da9787f5f46a1357b93fe9e19d1
masoud saed <masoudsaed918@gmail.com>
2023-03-18 20:19:55 +0000

    import component & Add them to App as element

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Services from './pages/services';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/index";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Contact from "./pages/contact-us";
+import Sign from "./pages/sign-up";
 
 function App() {
   return (
     <Router>
       <Navbar />
-        <Routes>
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/services' component={Services} />
-        </Routes>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/sign-up" element={<Sign />} />
+      </Routes>
     </Router>
   );
 }
