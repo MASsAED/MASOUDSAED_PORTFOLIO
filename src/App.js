c154a249ddd69d0b3ff0fd34efa8d576505a9d26
masoud saed <masoudsaed918@gmail.com>
2023-03-18 15:10:20 +0000

    added the pages to app

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages';
 import About from './pages/about';
+import Services from './pages/services';
 
 function App() {
   return (
     <Router>
       <Navbar />
         <Routes>
-        <Route path='/' exact component={ Home } />
-        <Route path='/about' component={ About } />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/services' component={Services} />
         </Routes>
     </Router>
   );
